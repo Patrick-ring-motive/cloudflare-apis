8d9c152d1ef05ab6e4fabbbdb96d7f879693fa45
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-02 21:05:01 -0500

    style: js-beautify formatting (unbound-server.js)

--- unbound-server.js
+++ unbound-server.js
@@ -12,14 +12,16 @@ const fetchResponse = async (...args) => {
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    url.hostname = url.hostname.replace(/-unbound/i,'');
+    url.hostname = url.hostname.replace(/-unbound/i, '');
     const unboundAPIKey = request.headers.get('unbound-api-key') || url.searchParams.get('unbound-api-key');
-    if(unboundAPIKey != env.UNBOUND_API_KEY){
-      return new Response(null,{status:403});
+    if (unboundAPIKey != env.UNBOUND_API_KEY) {
+      return new Response(null, {
+        status: 403
+      });
     }
     const domain = url.hostname.split('.').slice(1).join('.');
     const subdomain = request.headers.get('subdomain') || url.searchParams.get('subdomain');
     url.hostname = `${subdomain}.${domain}`;
-    return fetchResponse(String(url),request);
+    return fetchResponse(String(url), request);
   }
 };
